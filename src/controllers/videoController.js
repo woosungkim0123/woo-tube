a09827211e5f431e961ca1889a59026c716cbe6f
woosungkim0123 <sibu2005@naver.com>
2022-04-26 22:07:55 +0900

    database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -58,4 +58,16 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: `Upload Video` });
 };
 
-export const postUpload = (req, res) => res.redirect("/");
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
