1b67d1059d8f0052703acbc9e09287878183ad16
woosungkim0123 <sibu2005@naver.com>
2022-05-13 22:56:00 +0900

    public middleware

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -7,13 +7,18 @@ import {
   postJoin,
   postLogin,
 } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const rootRouter = express.Router();
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
-rootRouter.get("/logout", logout);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin);
+rootRouter.get("/logout", protectorMiddleware, logout);
 rootRouter.get("/search", search);
 
 export default rootRouter;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,12 +8,12 @@ import {
   see,
   startGithubLogin,
 } from "../controllers/userController";
+import { publicOnlyMiddleware } from "../middlewares";
 const userRouter = express.Router();
 
-userRouter.route("/edit").get(getEdit).post(postEdit);
-userRouter.get("/remove", remove);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
